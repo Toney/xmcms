50d88318318b1954a58cf19df90f6dc58a1287b6
朱君 <458820281@qq.com>
2015-07-03 09:30:32 +0800

    Signed-off-by: 朱君 <458820281@qq.com>

--- website/config/db.php
+++ website/config/db.php
@@ -1,4 +1,4 @@
 <?php
-$APP_DBSTR='mysql:host=root;port=3306;dbname=xmcms;autoReconnect=true';
+$APP_DBSTR='mysql:host=127.0.0.1;port=3306;dbname=xmcms;autoReconnect=true';
 $APP_USERNAME='root';
 $APP_PASSWORD='root';
