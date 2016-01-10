2537a3b55ea253e790366b3cfddb20cd28695a60
朱君 <458820281@qq.com>
2016-01-10 20:12:02 +0800

    1.数据库表类型修改 2.部分数据修正
    Signed-off-by: 朱君 <458820281@qq.com>

--- website/config/db.php
+++ website/config/db.php
@@ -1,4 +1,4 @@
 <?php
-$APP_DBSTR='mysql:host=localhost;port=3306;dbname=website;autoReconnect=true';
+$APP_DBSTR='mysql:host=localhost;port=3306;dbname=xmcms;autoReconnect=true';
 $APP_USERNAME='root';
 $APP_PASSWORD='root';
